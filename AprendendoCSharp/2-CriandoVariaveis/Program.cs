ebbd1ffc1334c0e70cbf473886cec31f17a42be5
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-06 21:55:05 -0300

    Compilando terceiro projeto

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+            int idade;
+            idade = 32;
+            Console.WriteLine("Sua idade é de "+idade+"!");
+
+            Console.WriteLine("Exeção finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+            Console.WriteLine(idade);
+
+            double Idade;
+            Idade = 15 / 2;
+            Console.WriteLine(Idade);
+
+            Console.WriteLine("A execução terminou, tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
