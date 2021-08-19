53d96ff6f83e6bb7953040276467ea0d3a830276
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-19 13:16:45 -0300

    Exercício 11 laços com FOR

---
+++ AprendendoCSharp/P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11 - Calula Poupança");
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *=  1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
