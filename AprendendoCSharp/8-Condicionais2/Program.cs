cc9d5c6374c03674537c49364352fc3cc257ebda
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-19 05:57:13 -0300

    Exercício 8 booleano compilado

---
+++ AprendendoCSharp/8-Condicionais2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("João Pode entrar.");
+            }
+            else
+            {
+                
+                Console.WriteLine("João Não pode entrar.");
+                
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
