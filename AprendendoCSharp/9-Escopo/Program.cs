ffe3823c06e65a75e4581a74d5ead094b110b0cf
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-19 06:48:41 -0300

    Exercício 9 mensagem adicional com booleano

---
+++ AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 9 - Escopo");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+            string mensagemAdicional;
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado!";
+            }
+            
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("João Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+
+                Console.WriteLine("João Não pode entrar.");
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
