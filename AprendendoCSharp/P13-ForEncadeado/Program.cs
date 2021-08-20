a5ead1199af2c0bb142ca490429ebf1f30d78933
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-20 06:22:25 -0300

    Exercício 12 for encadeados parte 2

---
+++ AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 13 - For encadeado");
+
+
+            //Desenhando asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {                   
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+
+            //Uma forma diferente para desenhar os asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+               
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                    Console.Write("*");
+                
+                Console.WriteLine();
+            }   
+
+            Console.ReadLine();
+        }
+    }
+}
