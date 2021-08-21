c65a9630fc257a58b3cf98642e105fed3f201aef
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-21 16:07:53 -0300

    Exercício 1 Classes

---
+++ POO/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
