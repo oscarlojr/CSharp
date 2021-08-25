3a17a5d6d9709bae304e565da781a93c7a4932a9
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-08-25 07:09:43 -0300

    Exercício 7 - Construtores e membros estáticos

---
+++ POO/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+            Console.WriteLine(ContaCorrente.TottalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(867, 86712520);
+            Console.WriteLine(ContaCorrente.TottalDeContasCriadas);
+
+
+            Console.ReadLine();
+        }
+    }
+}
